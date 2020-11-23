28c67ae6d27a6cf3424e94701136238258f0f0b9
adnanshibli <adnannajem.24.95@gmail.com>
2020-11-23 16:09:36 +0200

    done

--- server.js
+++ server.js
@@ -3,43 +3,108 @@
 const express = require('express');
 require('dotenv').config();
 const server = express();
-
+const superagent = require('superagent');
+// cors added
 const cors = require('cors');
-
 server.use(cors());
 
-const PORT = process.env.PORT || 3030;
+
+const PORT = process.env.PORT;
 
 server.get('/', (req, res) => {
-  res.send('Welcome to City Explorer');
-});
-server.get('/location', (req, res) => {
-  const locationData = require('./data/location.json');
-  const locationObject = new Location('Lynnwood' , locationData);
-  res.send(locationObject);
+  res.send('Homepage');
 });
 
-server.get('/weather', (req, res) => {
-  const weatehrData = require('./data/weather.json');
-  weatehrData.data.forEach((w) => {
-    let weatherObject = new Weather(w);
-  });
-  res.send(Weather.all);
-});
+server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+server.get('/trails', trailHandler);
+
+function locationHandler(req, res){
+  let cityName = req.query.city;
+  let locationToken =  `pk.4ebb1b076ca9a47134007045d8451279`;
+  let url = `https://us1.locationiq.com/v1/search.php?key=${locationToken}&q=${cityName}&format=json`;
+
+  superagent.get(url)
+    .then(data => {
+      const locationObject = new Location(cityName, data.body);
+      res.status(200).send(locationObject);
+    })
+    .catch(()=> {
+      errorHandler('Location .. Something went wrong!!', req, res);
+    });
+}
+
+function weatherHandler(req, res){
+  let cityName = req.query.search_query;
+  let cityLat = req.query.lat;
+  let cityLon = req.query.lon;
+
+  console.log(req.query);
+  let weatherKey = `b05bc3fe9a5844d0a937247968a69a67`;
+  //let weatherKey = process.env.WEATHER_API_KEY;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${weatherKey}&days=8&lat=${cityLat}&lon=${cityLon}`;
+  superagent.get(url)
+    .then(weatehrData => {
+      let weatherObjcts =  weatehrData.body.data.map( w => {
+        let weatherObject = new Weather(w);
+        return weatherObject;
+      });
+      res.status(200).json(weatherObjcts);
+    })
+    .catch(()=> {
+      errorHandler('Weather .. Something went wrong!!', req, res);
+    });
+}
+
+
+function trailHandler(req, res){
+  let trailLat = req.query.latitude;
+  let trailLon = req.query.longitude;
+  let trailsKey = `200980948-b2cc536f0eba97ded2aa594599ec62cd`;
+  // let trailsKey = process.env.TRAIL_API_KEY;
+  let url = `https://www.hikingproject.com/data/get-trails?lat=${trailLat}&lon=${trailLon}&maxDistance=10&key=${trailsKey}`;
+
+  superagent.get(url)
+    .then(tarilsData => {
+      console.log(JSON.parse(tarilsData.text).trails);
+      let trailObjects = JSON.parse(tarilsData.text).trails.map( t => {
+        let trail = new Trail(t);
+        return trail;
+      });
+      res.status(200).json(trailObjects);
+    })
+    .catch(() => {
+      errorHandler('Trails .. Something went wrong!!', req, res);
+    });
+}
+
+function errorHandler(error, req, res) {
+  res.status(500).send(error);
+}
 
 function Location(city, locationData){
   this.search_query = city;
   this.formatted_query = locationData[0].display_name;
   this.latitude = locationData[0].lat;
-  this.longtitude = locationData[0].lon;
-}
-Weather.all = [];
-function Weather(weatehrData){
-  this.forecast = weatehrData.weather.description;
-  this.time = weatehrData.valid_date;
-  Weather.all.push(this);
+  this.longitude = locationData[0].lon;
 }
 
+function Weather(weatehrData){
+  this.forecast = weatehrData.weather.description;
+  this.time = weatehrData.datetime;
+}
+function Trail(trailData){
+  this.name = trailData.name;
+  this.location = trailData.location;
+  this.length = trailData.length;
+  this.stars = trailData.stars;
+  this.star_votes = trailData.starVotes;
+  this.summary = trailData.summary;
+  this.trail_url = trailData.url;
+  this. conditions = trailData.conditionStatus;
+  this.condition_date = trailData.conditionDate.split(' ')[0];
+  this.condition_time = trailData.conditionDate.split(' ')[1];
+}
 server.get('*', (req, res) => {
   res.status(400).send('Not found');
 });
@@ -50,3 +115,5 @@ server.use((error, req, res) => {
 server.listen(PORT, ()=>{
   console.log(`Listening on port ${PORT}`);
 });
+
+
